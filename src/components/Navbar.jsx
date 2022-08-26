93a70264554c6e586fc05a1fd197807efccbd467
Rajkumar_Telukuntla <rajkumar_telukuntla@epam.com>
2022-08-27 01:08:57 +0530

    Added the urls to LinkedIn and Github

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,7 +92,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-900">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://www.linkedin.com/in/rajkumartk/"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -100,7 +100,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://github.com/rajkumar-telu"
             >
               Github <FaGithub size={30} />
             </a>
